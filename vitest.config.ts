9af77fbd49185243ec59c337ccdf36d86a786c7b
longdh-2048 <longdh.dev@gmail.com>
2025-06-22 09:46:24 +0700

    chore: update coverage include files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    coverage: {
+      include: ['src/**'],
+    },
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
   },
